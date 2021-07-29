6818cd9341b0bb048595484eb0015ea060b723d5
Steven Smith <steven_smith@worksafe.vic.gov.au>
2021-07-29 10:53:10 +1000

    Font preload

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="is-flex is-flex-spread">
-    <h2 class="is-light">Rick &amp; Morty</h2>
+    <img src="../assets/Rick_and_Morty.png" width="150">
     <div class="search-input-wrapper is-relative is-inline-flex">
       <input type="text" class="input" name="search" placeholder="Search...">
       <font-awesome-icon :icon="['fa', 'search']" class="is-absolute" style="top: 2px; right: 12px;" />
